fa62af01a8372063a5ba3a45d1426bb5eb0427a4
Марышев Андрей Владимирович <maryshev_av@rtsoft.ru>
2024-11-16 23:40:43 +0300

    first commit

---
+++ IGasStation2/Components/LabelAndTextBox.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IGasStation2.Components
+{
+    /// <summary>
+    /// Interaction logic for LabelAndTextBox.xaml
+    /// </summary>
+    public partial class LabelAndTextBox : UserControl
+    {
+        public LabelAndTextBox()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ControlLabelContentProperty =
+        DependencyProperty.Register("ControlLabelContent",
+                                    typeof(string),
+                                    typeof(LabelAndTextBox));
+
+        public string ControlLabelContent
+        {
+            get { return (string)GetValue(ControlLabelContentProperty); }
+            set { SetValue(ControlLabelContentProperty, value); }
+        }
+    }
+}
